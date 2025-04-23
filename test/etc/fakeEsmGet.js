dd38c1fd7728b2a0f85d00c1c24628b9de445a9e
Jon Jensen <jonj@netflix.com>
2025-04-22 18:19:53 -0600

    chore: lint fix

--- test/etc/fakeEsmGet.js
+++ test/etc/fakeEsmGet.js
@@ -3,10 +3,10 @@
 // simulate a misbehaving bundler
 
 function fakeEsmGet(req, res, next) {
-  res.header('name', 'fakeEsm');
-  res.header('method', 'get');
-  res.send(200);
-  next();
+    res.header('name', 'fakeEsm');
+    res.header('method', 'get');
+    res.send(200);
+    next();
 }
 fakeEsmGet.__esModule = true;
 fakeEsmGet.default = 'not a function';
