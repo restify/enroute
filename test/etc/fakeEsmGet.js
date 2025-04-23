a676ce18a5e6338e8811a3e2849042fa56334526
Jon Jensen <jonj@netflix.com>
2025-04-23 09:10:34 -0600

    fix: improve module type detection (#34)
    * fix: improve module type detection
    
    minor tweak to handling to account for misbehaving bundlers
    
    * chore: lint fix

---
+++ test/etc/fakeEsmGet.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// simulate a misbehaving bundler
+
+function fakeEsmGet(req, res, next) {
+    res.header('name', 'fakeEsm');
+    res.header('method', 'get');
+    res.send(200);
+    next();
+}
+fakeEsmGet.__esModule = true;
+fakeEsmGet.default = 'not a function';
+
+module.exports = fakeEsmGet;
